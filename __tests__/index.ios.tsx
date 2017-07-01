16e1d6061c1d0bddb243d38b678307ede444fc3f
Izik Lisbon <izikl@microsoft.com>
2017-07-01 02:43:27 -0700

    introduce typescript

--- __tests__/index.ios.tsx
+++ __tests__/index.ios.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Index from '../index.ios.js';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import * as renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
