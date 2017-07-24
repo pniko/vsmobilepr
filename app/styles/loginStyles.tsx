809f1221fa5e02712f1c23ae98f3280d64f52f81
chrwend <chwend@microsoft.com>
2017-07-24 20:19:21 +0200

    Display projects list view after login

--- app/styles/loginStyles.tsx
+++ app/styles/loginStyles.tsx
@@ -3,9 +3,9 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   input: {
     height: 40, 
@@ -13,5 +13,5 @@ export default StyleSheet.create({
     borderWidth: 1,
     margin: 20,
     padding: 10
-  },
+  }
 });
