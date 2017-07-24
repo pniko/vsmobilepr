433e59cf6bcaa803dfabd90212aa7e7cd4c04909
chrwend <chwend@microsoft.com>
2017-07-24 20:18:25 +0200

    Add list row component

---
+++ app/components/listRow.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import styles from '../styles/listRowStyles';
+
+class ListRowProps {
+  title: string;
+}
+
+export class ListRow extends Component<ListRowProps, {}> {
+
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text style={styles.text}>{this.props.title}dsa</Text>
+      </View>
+    );
+  }
+}
---
+++ app/styles/listRowStyles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    height: 44,
+    backgroundColor: 'white',
+    padding: 10,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  text: {
+    flex: 1,
+    color: 'black',
+    fontSize: 20
+  }
+});
