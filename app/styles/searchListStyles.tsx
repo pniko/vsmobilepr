565ee7cadff898fdd78d15d81c289fbb5121187b
chrwend <chwend@microsoft.com>
2017-07-24 20:18:45 +0200

    Add abstract base list view

---
+++ app/styles/searchListStyles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listcontainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  list: {
+    flex: 1
+  },
+  searchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10
+  }
+});
